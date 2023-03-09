9d4ea11d006e966f0358c41b6f0dc4ab5a8fb43d
Thiago Nascimento <nascimentoliveira@gmail.com>
2023-03-09 15:36:50 -0300

    feat: event on redis

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,18 @@
-import { prisma } from "@/config";
+import { prisma, redis } from "@/config";
+import { Event } from "@prisma/client";
+
+const cacheKey = "event";
+const EXPIRATION = 60*60;
 
 async function findFirst() {
-  return prisma.event.findFirst();
+  const data: Event = JSON.parse(await redis.get(cacheKey));
+  if (data != null) {
+    return data;
+  }
+
+  const freshData = await prisma.event.findFirst();
+  redis.setEx(cacheKey, EXPIRATION, JSON.stringify(freshData));
+  return freshData;
 }
 
 const eventRepository = {
